4afb05d7ba28e579b85585af09efd86fc757f34e
ArzuEl1yeva <arzueliyeva0212@gmail.com>
2025-04-01 01:21:31 +0400

    Add files via upload

---
+++ Convert Celsius to Fahrenheit.py
@@ -0,0 +1,7 @@
+#Convert Celsius to Fahrenheit
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+# Example
+print(celsius_to_fahrenheit(0))   # Output: 32.0
+print(celsius_to_fahrenheit(100)) # Output: 212.0
